f8ffa211b9153876d68865b2307dca0ced8f31ea
nashr <muhammad.nassirudin@gmail.com>
2014-05-07 07:30:20 +0700

    Adding lamp code to door and a little roll out

--- The-Rooms/src/model.py
+++ The-Rooms/src/model.py
@@ -10,48 +10,56 @@ class Maze:
 		self.row = row
 		self.col = col
 		
+		# What's inside the room? (0: nothing; 1: bomb; 2: plant)
 		self.rooms = []
 		
 		for i in range(row * col):
-			self.rooms.append(1)
+			self.rooms.append(0)
+
+		# Room's lamp's current light. (0: green; 1: grey; 2: red)
+		self.lights = []
+		
+		for i in range(row * col):
+			self.lights.append(1)
 
 		self.player = Player(-1, -1)
 
 	def load_curr_room_property(self):
 		# Is there a door (at) [ahead, right, back, left]
-		retval = [False, False, False, False]
+		# Code: -1: no door; 0: door with green light; 1: door with grey light; 2: door with red light
+		retval = [-1, -1, -1, -1]
 
-		if self.player.r == -1:
+		if self.player.r == -1: # Only in menu context
 			pass
-		elif self.player.r == 0:
-			retval[(2 - self.player.d) % 4] = True
-			if self.player.c == 0:
-				retval[(1 - self.player.d) % 4] = True
-			elif self.player.c == self.col - 1:
-				retval[(3 - self.player.d) % 4] = True
-			else:
-				retval[(1 - self.player.d) % 4] = True
-				retval[(3 - self.player.d) % 4] = True
-		elif self.player.r == self.row - 1:
-			retval[(0 - self.player.d) % 4] = True
-			if self.player.c == 0:
-				retval[(1 - self.player.d) % 4] = True
-			elif self.player.c == self.col - 1:
-				retval[(3 - self.player.d) % 4] = True
-			else:
-				retval[(1 - self.player.d) % 4] = True
-				retval[(3 - self.player.d) % 4] = True
-		else:
-			retval[(0 - self.player.d) % 4] = True
-			retval[(2 - self.player.d) % 4] = True
-			if self.player.c == 0:
-				retval[(1 - self.player.d) % 4] = True
-			elif self.player.c == self.col - 1:
-				retval[(3 - self.player.d) % 4] = True
-			else:
-				retval[(1 - self.player.d) % 4] = True
-				retval[(3 - self.player.d) % 4] = True
-		
+		else: # In game context
+			if self.player.r == 0: # The most up rooms
+				room_idx = (self.player.r + 1)* self.row + self.player.c
+				retval[(2 - self.player.d) % 4] = self.lights[room_idx]
+			elif self.player.r == self.row - 1: # The most down rooms
+				room_idx = (self.player.r - 1)* self.row + self.player.c
+				retval[(0 - self.player.d) % 4] = self.lights[room_idx]
+			else: # Middle row rooms
+				# Head door
+				room_idx = (self.player.r - 1)* self.row + self.player.c
+				retval[(0 - self.player.d) % 4] = self.lights[room_idx]
+				# Back door
+				room_idx = (self.player.r + 1)* self.row + self.player.c
+				retval[(2 - self.player.d) % 4] = self.lights[room_idx]
+			
+			if self.player.c == 0: # The most left rooms
+				room_idx = self.player.r * self.row + (self.player.c + 1)
+				retval[(1 - self.player.d) % 4] = self.lights[room_idx]
+			elif self.player.c == self.col - 1: # The most right rooms
+				room_idx = self.player.r * self.row + (self.player.c - 1)
+				retval[(3 - self.player.d) % 4] = self.lights[room_idx]
+			else: # Middle column rooms
+				# Right door
+				room_idx = self.player.r * self.row + (self.player.c + 1)
+				retval[(1 - self.player.d) % 4] = self.lights[room_idx]
+				# Left door
+				room_idx = self.player.r * self.row + (self.player.c - 1)
+				retval[(3 - self.player.d) % 4] = self.lights[room_idx]
+
 		return retval
 	
 	def generate_rooms(self):
